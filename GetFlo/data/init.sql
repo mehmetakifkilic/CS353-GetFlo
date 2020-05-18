fb846c043e0f68bb2904aea70db6f34ed87b2293
[ahmetberkeren] <[berk.eren@ug.bilkent.edu.tr]>
2020-05-18 19:18:07 +0300

    Login and Signup page added

--- GetFlo/data/init.sql
+++ GetFlo/data/init.sql
@@ -2,12 +2,40 @@ CREATE DATABASE test;
 
   use test;
 
-  CREATE TABLE customers(
-    customerID INT PRIMARY KEY AUTO_INCREMENT,
-    username VARCHAR(255) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL,
-    name VARCHAR(255) NOT NULL,
-    gender VARCHAR(255),
-    phone_number VARCHAR(20) NOT NULL
+    CREATE TABLE users(
+        ID INT PRIMARY KEY,
+        username VARCHAR(255) NOT NULL UNIQUE ,
+        password VARCHAR(255) NOT NULL,
+        type VARCHAR(255) NOT NULL
     );
-ALTER TABLE customers AUTO_INCREMENT=1001;
+    CREATE TABLE customers(
+        name VARCHAR(255) NOT NULL,
+        gender VARCHAR(255),
+        phone_number VARCHAR(20) NOT NULL,
+        customerID INT PRIMARY KEY AUTO_INCREMENT REFERENCES users(ID) ON DELETE CASCADE ,
+        password VARCHAR(255) REFERENCES users(password) ON DELETE CASCADE ,
+        username VARCHAR(255) REFERENCES users(username) ON DELETE CASCADE
+    );
+    ALTER TABLE customers AUTO_INCREMENT = 1001;
+    CREATE TABLE flowersellers(
+        company_name varchar(255) NOT NULL,
+        rating float(8,4),
+        people_rated int,
+        phone_number varchar(20) NOT NULL,
+        sellerID INT PRIMARY KEY AUTO_INCREMENT REFERENCES users(ID) ON DELETE CASCADE ,
+        username varchar(255) NOT NULL UNIQUE REFERENCES users(username) ON DELETE CASCADE ,
+        password varchar(255) NOT NULL REFERENCES  users(password) ON DELETE CASCADE
+    );
+    ALTER TABLE flowersellers AUTO_INCREMENT = 2001;
+    CREATE TABLE couriers(
+        name varchar(255) NOT NULL,
+        rating float(8,4),
+        people_rated int,
+        phone_number varchar(20) NOT NULL,
+        courierID INT PRIMARY KEY AUTO_INCREMENT REFERENCES users(ID) ON DELETE CASCADE ,
+        username varchar(255) NOT NULL UNIQUE REFERENCES users(username) ON DELETE CASCADE ,
+        password varchar(255) NOT NULL REFERENCES users(password) ON DELETE CASCADE
+    );
+    ALTER TABLE couriers AUTO_INCREMENT = 3001;
+
+
--- GetFlo/public/index.php
+++ GetFlo/public/index.php
@@ -1,16 +1,17 @@
 <?php include "templates/header.php";
-if (isset($_POST['submit'])) {
+require "../common.php";
+if (isset($_POST['submitCustomer'])) {
     require "../config.php";
 
     try {
         $connection = new PDO($dsn, $username, $password, $options);
 
         $new_user = array(
-            "name" => $_POST['name'],
-            "username"  => $_POST['username'],
-            "password"     => $_POST['password'],
-            "gender"       => $_POST['gender'],
-            "phone_number"       => $_POST['phonenumber']
+            "name" => $_POST['cusname'],
+            "username"  => $_POST['cususername'],
+            "password"     => $_POST['cuspassword'],
+            "gender"       => $_POST['cusgender'],
+            "phone_number"       => $_POST['cusphonenumber']
         );
 
         $sql = sprintf(
@@ -23,29 +24,270 @@ if (isset($_POST['submit'])) {
         $statement = $connection->prepare($sql);
         $statement->execute($new_user);
 
+        $sql = "SELECT * FROM customers WHERE username = :username AND password = :password";
+
+        $signupusername = $_POST['cususername'];
+        $signuppassword = $_POST['cuspassword'];
+
+        $statement = $connection->prepare($sql);
+        $statement->bindParam(':username', $signupusername, PDO::PARAM_STR);
+        $statement->bindParam(':password', $signuppassword, PDO::PARAM_STR);
+        $statement->execute();
+
+        $result = $statement->fetchAll();
+        foreach ($result as $row)
+            $result2 = $row['customerID'];
+
+        $new_user = array(
+            "ID" => $result2,
+            "username"  => $_POST['cususername'],
+            "password"     => $_POST['cuspassword'],
+            "type" => "Customer"
+        );
+        $sql = sprintf(
+            "INSERT INTO %s (%s) values (%s)",
+            "users",
+            implode(", ", array_keys($new_user)),
+            ":" . implode(", :", array_keys($new_user))
+        );
+
+        $statement = $connection->prepare($sql);
+        $statement->execute($new_user);
     } catch(PDOException $error) {
         echo $sql . "<br>" . $error->getMessage();
     }
 
 }
+
+if (isset($_POST['submitSeller'])) {
+    require "../config.php";
+
+    try {
+        $connection = new PDO($dsn, $username, $password, $options);
+
+        $new_user = array(
+            "company_name" => $_POST['selname'],
+            "username"  => $_POST['selusername'],
+            "password"     => $_POST['selpassword'],
+            "rating"       => 0.0,
+            "people_rated" => 0,
+            "phone_number"      => $_POST['selphonenumber']
+        );
+
+        $sql = sprintf(
+            "INSERT INTO %s (%s) values (%s)",
+            "flowersellers",
+            implode(", ", array_keys($new_user)),
+            ":" . implode(", :", array_keys($new_user))
+        );
+
+        $statement = $connection->prepare($sql);
+        $statement->execute($new_user);
+
+
+        $sql = "SELECT * FROM flowersellers WHERE username = :username AND password = :password";
+
+        $signupusername = $_POST['selusername'];
+        $signuppassword = $_POST['selpassword'];
+
+        $statement = $connection->prepare($sql);
+        $statement->bindParam(':username', $signupusername, PDO::PARAM_STR);
+        $statement->bindParam(':password', $signuppassword, PDO::PARAM_STR);
+        $statement->execute();
+
+        $result = $statement->fetchAll();
+        foreach ($result as $row)
+            $result2 = $row['sellerID'];
+
+        $new_user = array(
+            "ID" => $result2,
+            "username"  => $_POST['selusername'],
+            "password"     => $_POST['selpassword'],
+            "type" => "Seller"
+        );
+        $sql = sprintf(
+            "INSERT INTO %s (%s) values (%s)",
+            "users",
+            implode(", ", array_keys($new_user)),
+            ":" . implode(", :", array_keys($new_user))
+        );
+
+        $statement = $connection->prepare($sql);
+        $statement->execute($new_user);
+
+    } catch(PDOException $error) {
+        echo $sql . "<br>" . $error->getMessage();
+    }
+
+}
+
+if (isset($_POST['submitCourier'])) {
+    require "../config.php";
+
+    try {
+        $connection = new PDO($dsn, $username, $password, $options);
+
+        $new_user = array(
+            "name" => $_POST['couname'],
+            "username"  => $_POST['couusername'],
+            "password"     => $_POST['coupassword'],
+            "rating"       => 0.0,
+            "people_rated" => 0,
+            "phone_number"      => $_POST['couphonenumber']
+        );
+
+        $sql = sprintf(
+            "INSERT INTO %s (%s) values (%s)",
+            "couriers",
+            implode(", ", array_keys($new_user)),
+            ":" . implode(", :", array_keys($new_user))
+        );
+
+        $statement = $connection->prepare($sql);
+        $statement->execute($new_user);
+
+        $sql = "SELECT * FROM couriers WHERE username = :username AND password = :password";
+
+        $signupusername = $_POST['couusername'];
+        $signuppassword = $_POST['coupassword'];
+
+        $statement = $connection->prepare($sql);
+        $statement->bindParam(':username', $signupusername, PDO::PARAM_STR);
+        $statement->bindParam(':password', $signuppassword, PDO::PARAM_STR);
+        $statement->execute();
+
+        $result = $statement->fetchAll();
+        foreach ($result as $row)
+            $result2 = $row['courierID'];
+
+        $new_user = array(
+            "ID" => $result2,
+            "username"  => $_POST['couusername'],
+            "password"     => $_POST['coupassword'],
+            "type" => "Courier"
+        );
+        $sql = sprintf(
+            "INSERT INTO %s (%s) values (%s)",
+            "users",
+            implode(", ", array_keys($new_user)),
+            ":" . implode(", :", array_keys($new_user))
+        );
+
+        $statement = $connection->prepare($sql);
+        $statement->execute($new_user);
+
+    } catch(PDOException $error) {
+        echo $sql . "<br>" . $error->getMessage();
+    }
+
+}
+
+if (isset($_POST['submitLogin'])) {
+    try {
+        require "../config.php";
+        require "../common.php";
+
+        $connection = new PDO($dsn, $username, $password, $options);
+
+        $sql = "SELECT *
+        FROM users
+        WHERE username = :username AND password = :password";
+
+        $loginusername = $_POST['loginusername'];
+        $loginpassword = $_POST['loginpassword'];
+
+        $statement = $connection->prepare($sql);
+        $statement->bindParam(':username', $loginusername, PDO::PARAM_STR);
+        $statement->bindParam(':password', $loginpassword, PDO::PARAM_STR);
+        $statement->execute();
+
+        $result = $statement->fetchAll();
+
+        foreach ($result as $row) {
+            $type = $row['type'];
+        }
+        if($type == "Customer")
+            header("Location: /customermainpage.php");
+        if($type == "Seller")
+            header("Location: /sellermainpage.php");
+        if($type == "Courier")
+            header("Location: /couriermainpage.php");
+        if($type == "Service")
+            header("Location: /servicemainpage.php");
+        if($type == "Grower")
+            header("Location: /growermainpage.php");
+        else echo "Invalid Username and Password!";
+    } catch(PDOException $error) {
+        echo $sql . "<br>" . $error->getMessage();
+    }
+}
 ?>
 
     <ul>
+        <?php if ((isset($_POST['submitCustomer']) || isset($_POST['submitSeller']) || isset($_POST['submitCourier'])) && $statement) { ?>
+            > <?php echo $new_user['username']; ?> successfully added.
+        <?php } ?>
         <li>
-            <h2>Add a user</h2>
+            <h2>Login</h2>
+
+            <form method="post">
+                <label for="username">Username</label>
+                <input type="text" name="loginusername" id="loginusername">
+                <label for="loginpassword">Password</label>
+                <input type="text" name="loginpassword" id="loginpassword">
+                <input type="submit" name="submitLogin" value="Login">
+            </form>
+
+        </li>
+
+        <li>
+            <h2>Sign Up As Customer</h2>
 
             <form method="post">
                 <label for="name">First-Last Name</label>
-                <input type="text" name="name" id="name">
+                <input type="text" name="cusname" id="cusname">
                 <label for="username">Username</label>
-                <input type="text" name="username" id="username">
+                <input type="text" name="cususername" id="cususername">
                 <label for="password">Password</label>
-                <input type="text" name="password" id="password">
+                <input type="text" name="cuspassword" id="cuspassword">
                 <label for="gender">Gender</label>
-                <input type="text" name="gender" id="gender">
+                <input type="text" name="cusgender" id="cusgender">
                 <label for="phonenumber">Phone Number</label>
-                <input type="text" name="phonenumber" id="phonenumber">
-                <input type="submit" name="submit" value="Submit">
+                <input type="text" name="cusphonenumber" id="cusphonenumber">
+                <input type="submit" name="submitCustomer" value="Sign Up">
+            </form>
+
+        </li>
+        <li>
+            <h2>Sign Up As Flower Seller</h2>
+
+            <form method="post">
+                <label for="selname">Company Name</label>
+                <input type="text" name="selname" id="selname">
+                <label for="selusername">Username</label>
+                <input type="text" name="selusername" id="selusername">
+                <label for="selpassword">Password</label>
+                <input type="text" name="selpassword" id="selpassword">
+                <label for="selphonenumber">Phone Number</label>
+                <input type="text" name="selphonenumber" id="selphonenumber">
+                <input type="submit" name="submitSeller" value="Sign Up">
+            </form>
+
+        </li>
+
+        <li>
+            <h2>Sign Up As Courier</h2>
+
+            <form method="post">
+                <label for="couname">Name</label>
+                <input type="text" name="couname" id="couname">
+                <label for="couusername">Username</label>
+                <input type="text" name="couusername" id="couusername">
+                <label for="coupassword">Password</label>
+                <input type="text" name="coupassword" id="coupassword">
+                <label for="couphonenumber">Phone Number</label>
+                <input type="text" name="couphonenumber" id="couphonenumber">
+                <input type="submit" name="submitCourier" value="Sign Up">
             </form>
 
         </li>
